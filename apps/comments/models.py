5be92d9962ec9b872af8479901643d212cc605c5
denis.sh <denniskitun@gmail.com>
2018-04-16 17:32:29 +0300

    created comments app; created comment model

---
+++ apps/comments/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from apps.posts.models import Post
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, null=True, on_delete=models.CASCADE)
+
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return str(self.pk) + ' - ' + str(self.author) + ': "' + str(self.content) + '"'
+
+
+
+
+
