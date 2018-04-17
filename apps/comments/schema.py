42f53892c15da39742179b35b4b8f61d89241062
denis.sh <denniskitun@gmail.com>
2018-04-17 12:29:15 +0300

    added posts and comments search

--- apps/comments/schema.py
+++ apps/comments/schema.py
@@ -1,5 +1,7 @@
 import graphene
+from django.db.models import Q
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
 
 from .models import Comment
 from apps.users.schema import UserType
@@ -22,32 +24,43 @@ class CreateComment(graphene.Mutation):
         post = graphene.Int()
         content = graphene.String()
 
-    def mutate(self, info, post, content):
+    def mutate(self, info, post=None, content=None):
         user = info.context.user or None
 
         if user.is_anonymous:
-            raise Exception('You must be logged to vote!')
+            raise GraphQLError('You must be logged to comment!')
 
-        cur_post = Post.objects.filter(id=post).first()
-        if not cur_post:
+        if not post:
+            raise GraphQLError('You don`t specify the post id!')
+
+        if not content:
+            raise GraphQLError('Comment shouldn`t be empty!')
+
+        post_obj = Post.objects.filter(id=post).first()
+
+        if not post_obj:
             raise Exception('Invalid Link!')
 
-        comment = Comment.objects.create(post=cur_post,
+        comment = Comment.objects.create(post=post_obj,
                           content=content,
                           author=user)
 
         return CreateComment(
             id=comment.pk,
-            post=cur_post,
+            post=post_obj,
             content=comment.content,
             author=comment.author,
         )
 
 
 class Query(graphene.ObjectType):
-    comments = graphene.List(CommentType)
 
-    def resolve_comments(self, info, **kwargs):
+    comments = graphene.List(CommentType, search=graphene.String())
+
+    def resolve_comments(self, info, search=None):
+        if search:
+            filter = Q(content__icontains=search)
+            return Comment.objects.filter(filter)
         return Comment.objects.all()
 
 
--- apps/posts/schema.py
+++ apps/posts/schema.py
@@ -1,9 +1,11 @@
 import graphene
 from graphene_django import DjangoObjectType
+from django.db.models import Q
+from graphql import GraphQLError
 
 from .models import Post
 from apps.users.schema import UserType
-from apps.comments.models import Comment
+
 
 class PostType(DjangoObjectType):
     class Meta:
@@ -11,20 +13,26 @@ class PostType(DjangoObjectType):
 
 
 class CreatePost(graphene.Mutation):
-    import apps.comments.schema
 
     id = graphene.Int()
     title = graphene.String()
     content = graphene.String()
     author = graphene.Field(UserType)
-    # comments = graphene.Field(apps.comments.schema.CommentType)
 
     class Arguments:
         title = graphene.String()
         content = graphene.String()
 
-    def mutate(self, info, title, content):
+    def mutate(self, info, title=None, content=None, **kwargs):
         user = info.context.user or None
+
+        if not title and not content:
+            raise GraphQLError('You must be forget to provide post title and content!')
+        elif not title:
+            raise GraphQLError('You must be forget to provide post title!')
+        elif not content:
+            raise GraphQLError('Post content shouldn`t be empty!')
+
         post = Post(title=title,
                     content=content,
                     author=user)
@@ -34,22 +42,27 @@ class CreatePost(graphene.Mutation):
             id=post.pk,
             title=post.title,
             content=post.content,
-            # author=post.author,
-            # comments=None
+            author=post.author,
         )
 
 
 class Query(graphene.ObjectType):
-    import apps.comments.schema
 
-    posts = graphene.List(PostType)
-    comments = graphene.List(apps.comments.schema.CommentType)
+    posts = graphene.List(PostType, search=graphene.String(), id=graphene.ID())
 
-    def resolve_posts(self, info, **kwargs):
+    def resolve_posts(self, info, id=None, search=None, **kwargs):
+        if id:
+            filter = (
+                Q(pk__iexact=id)
+            )
+            return Post.objects.filter(filter)
+        if search:
+            filter = (
+                Q(title__icontains=search) |
+                Q(content__icontains=search)
+            )
+            return Post.objects.filter(filter)
         return Post.objects.all()
-
-    def resolve_comments(self, info, **kwargs):
-        return Comment.objects.all()
 
 
 class Mutation(graphene.ObjectType):
